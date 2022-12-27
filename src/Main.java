03152394aa4078d84b717a19101bcf535d36648e
MaxTsarev <maxtsar33@gmail.com>
2022-12-27 10:11:47 +0300

    Final version

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] sales = {5, 2, 4, 7};
+
+        SalesManager show = new SalesManager(sales);
+
+        System.out.println("В январе больше всего было продано автомобилей в " + show.indexMax() + " неделю месяца: " + show.max() + " шт.");
+
+    }
+}
